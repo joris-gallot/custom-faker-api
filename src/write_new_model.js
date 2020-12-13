88f71f17630808071b3f07b9f94274bbf1078c77
Joris <you@example.com>
2020-12-13 18:09:35 +0100

    add promise

--- src/write_new_model.js
+++ src/write_new_model.js
@@ -1,79 +1,93 @@
 const fs = require('fs')
 
 module.exports = (model, attributes) => {
-  writeModel(model, attributes)
-  writeController(model)
-  writeRoute(model)
-  writeApp()
+  new Promise((resolve) => {
+    resolve(
+      writeModel(model, attributes),
+      writeController(model),
+      writeRoute(model)
+    )
+  })
+    .then(() => {
+      writeApp()
+    })
+    .catch((error) => {
+      console.error('ERROR: errors creating model routes')
+      console.error(error)
+      process.exit(1)
+    })
 }
 
 function writeModel(model, attributes) {
-  fs.readFile(`${__dirname}/server/models/base.model`, (err, data) => {
-    if (err) {
-      throw err
-    }
+  return new Promise((resolve, reject) => {
+    fs.readFile(`${__dirname}/server/models/base.model`, (err, data) => {
+      if (err) reject(err)
 
-    let contructAttributes = ''
-    for (const key in attributes) {
-      if (attributes.hasOwnProperty(key)) {
-        const fakerFuc = attributes[key]
-        contructAttributes += `this.${key}=faker.${fakerFuc}()\n`
+      let contructAttributes = ''
+      for (const key in attributes) {
+        if (attributes.hasOwnProperty(key)) {
+          const fakerFuc = attributes[key]
+          contructAttributes += `this.${key}=faker.${fakerFuc}()\n`
+        }
       }
-    }
 
-    const content = data
-      .toString()
-      .replace(/{{model}}/g, model)
-      .replace(/{{attributes}}/g, contructAttributes)
+      const content = data
+        .toString()
+        .replace(/{{model}}/g, model)
+        .replace(/{{attributes}}/g, contructAttributes)
 
-    fs.writeFile(`${__dirname}/server/models/${model}.js`, content, (err) => {
-      if (err) throw err
+      fs.writeFile(`${__dirname}/server/models/${model}.js`, content, (err) => {
+        if (err) reject(err)
 
-      console.log('The model file was succesfully saved!')
+        console.log('The model file was succesfully saved!')
+        resolve()
+      })
     })
   })
 }
 
 function writeController(model) {
-  fs.readFile(
-    `${__dirname}/server/controllers/base.controller`,
-    (err, data) => {
-      if (err) {
-        throw err
+  return new Promise((resolve, reject) => {
+    fs.readFile(
+      `${__dirname}/server/controllers/base.controller`,
+      (err, data) => {
+        if (err) reject(err)
+
+        const content = data.toString().replace(/{{model}}/g, model)
+
+        fs.writeFile(
+          `${__dirname}/server/controllers/${model}_controller.js`,
+          content,
+          (err) => {
+            if (err) reject(err)
+
+            console.log('The controller file was succesfully saved!')
+            resolve()
+          }
+        )
       }
+    )
+  })
+}
+
+function writeRoute(model) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(`${__dirname}/server/routes/base.route`, (err, data) => {
+      if (err) reject(err)
 
       const content = data.toString().replace(/{{model}}/g, model)
 
       fs.writeFile(
-        `${__dirname}/server/controllers/${model}_controller.js`,
+        `${__dirname}/server/routes/${model}_routes.js`,
         content,
         (err) => {
-          if (err) throw err
+          if (err) reject(err)
 
-          console.log('The controller file was succesfully saved!')
+          console.log('The route file was succesfully saved!')
+          resolve()
         }
       )
-    }
-  )
-}
-
-function writeRoute(model) {
-  fs.readFile(`${__dirname}/server/routes/base.route`, (err, data) => {
-    if (err) {
-      throw err
-    }
-
-    const content = data.toString().replace(/{{model}}/g, model)
-
-    fs.writeFile(
-      `${__dirname}/server/routes/${model}_routes.js`,
-      content,
-      (err) => {
-        if (err) throw err
-
-        console.log('The route file was succesfully saved!')
-      }
-    )
+    })
   })
 }
 
