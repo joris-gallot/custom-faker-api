6628d5aa8dfd98f4da09c08ccd09e4b1f6328054
Joris <you@example.com>
2020-12-25 14:57:47 +0100

    fix watch config file

--- clean-server.sh
+++ clean-server.sh
@@ -1,3 +1,3 @@
 rm src/server/routes/index.js
-rm src/config.JSON
+rm src/config.json
 echo "[]" >> src/config.json
--- src/watch_config.js
+++ src/watch_config.js
@@ -3,100 +3,121 @@ const fsPromises = fs.promises
 
 module.exports = () => {
   fs.watch(`${__dirname}/config.json`, (curr, prev) => {
-    writeModelFiles()
-    writeControllerFiles()
-    writeRouteFiles()
-    writeRoutesIndexFile()
+    fs.readFile(`${__dirname}/config.json`, (err, data) => {
+      const config = JSON.parse(data.toString())
+
+      Promise.all([
+        writeModelFiles(config),
+        writeControllerFiles(config),
+        writeRouteFiles(config),
+      ])
+        .then(() => {
+          writeRoutesIndexFile(config)
+        })
+        .catch((err) => {
+          console.error(`ERROR: error when creating routes`)
+          console.error(err)
+          process.exit(1)
+        })
+    })
   })
 }
 
-async function removeFiles(path) {
+async function removeFiles(path, reject) {
   const files = fs.readdirSync(`${__dirname}/server/${path}`).filter((file) => {
     return file.endsWith('.js')
   })
 
   for (const file of files) {
     await fsPromises.unlink(`${__dirname}/server/${path}/${file}`, (err) => {
-      if (err) throw err
+      if (err) reject(err)
     })
   }
 }
 
-async function writeModelFiles() {
-  const config = require('./config.json')
-  await removeFiles('models')
+function writeModelFiles(config) {
+  return new Promise(async (resolve, reject) => {
+    await removeFiles('models', reject)
 
-  fs.readFile(`${__dirname}/blueprints/model.bp`, (err, data) => {
-    if (err) throw err
+    fs.readFile(`${__dirname}/blueprints/model.bp`, (err, data) => {
+      if (err) reject(err)
 
-    for (const model of config) {
-      const name = model.name.toLocaleLowerCase()
+      for (const model of config) {
+        const name = model.name.toLocaleLowerCase()
 
-      let constructAttributes = ''
+        let constructAttributes = ''
 
-      model.attributes.forEach((attribute) => {
-        constructAttributes += `this.${attribute.name}=faker.${attribute.faker}()\n`
-      })
+        model.attributes.forEach((attribute) => {
+          constructAttributes += `this.${attribute.name}=faker.${attribute.faker}()\n`
+        })
 
-      const content = data
-        .toString()
-        .replace(/{{model}}/g, name)
-        .replace(/{{attributes}}/g, constructAttributes)
+        const content = data
+          .toString()
+          .replace(/{{model}}/g, name)
+          .replace(/{{attributes}}/g, constructAttributes)
 
-      fs.writeFile(`${__dirname}/server/models/${name}.js`, content, (err) => {
-        if (err) throw err
-      })
-    }
+        fs.writeFile(
+          `${__dirname}/server/models/${name}.js`,
+          content,
+          (err) => {
+            if (err) reject(err)
+            resolve()
+          }
+        )
+      }
+    })
   })
 }
 
-async function writeControllerFiles() {
-  const config = require('./config.json')
-  await removeFiles('controllers')
+function writeControllerFiles(config) {
+  return new Promise(async (resolve, reject) => {
+    await removeFiles('controllers', reject)
 
-  fs.readFile(`${__dirname}/blueprints/controller.bp`, (err, data) => {
-    if (err) throw err
+    fs.readFile(`${__dirname}/blueprints/controller.bp`, (err, data) => {
+      if (err) reject(err)
 
-    for (const model of config) {
-      const name = model.name.toLocaleLowerCase()
-      const content = data.toString().replace(/{{model}}/g, name)
+      for (const model of config) {
+        const name = model.name.toLocaleLowerCase()
+        const content = data.toString().replace(/{{model}}/g, name)
 
-      fs.writeFile(
-        `${__dirname}/server/controllers/${name}_controller.js`,
-        content,
-        (err) => {
-          if (err) throw err
-        }
-      )
-    }
+        fs.writeFile(
+          `${__dirname}/server/controllers/${name}_controller.js`,
+          content,
+          (err) => {
+            if (err) reject(err)
+            resolve()
+          }
+        )
+      }
+    })
   })
 }
 
-async function writeRouteFiles() {
-  const config = require('./config.json')
-  await removeFiles('routes')
+function writeRouteFiles(config) {
+  return new Promise(async (resolve, reject) => {
+    await removeFiles('routes', reject)
 
-  fs.readFile(`${__dirname}/blueprints/route.bp`, (err, data) => {
-    if (err) throw err
+    fs.readFile(`${__dirname}/blueprints/route.bp`, (err, data) => {
+      if (err) reject(err)
 
-    for (const model of config) {
-      const name = model.name.toLocaleLowerCase()
-      const content = data.toString().replace(/{{model}}/g, name)
+      for (const model of config) {
+        const name = model.name.toLocaleLowerCase()
+        const content = data.toString().replace(/{{model}}/g, name)
 
-      fs.writeFile(
-        `${__dirname}/server/routes/${name}_routes.js`,
-        content,
-        (err) => {
-          if (err) throw err
-        }
-      )
-    }
+        fs.writeFile(
+          `${__dirname}/server/routes/${name}_routes.js`,
+          content,
+          (err) => {
+            if (err) reject(err)
+            resolve()
+          }
+        )
+      }
+    })
   })
 }
 
-function writeRoutesIndexFile() {
-  const config = require('./config.json')
-
+function writeRoutesIndexFile(config) {
   let body = ''
   for (const model of config) {
     const name = model.name.toLocaleLowerCase()
